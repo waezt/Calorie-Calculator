6a87daf8e326e3b41dd4c50eb2c060469d892b08
Waez <waeztariq@icloud.com>
2025-11-21 18:05:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
